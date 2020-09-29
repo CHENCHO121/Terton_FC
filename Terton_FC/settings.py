72d11ca677b8ed2518cd7c3213bfb9eea29c4c00
CHENCHO121 <chenchogyeltshen000@gmail.com>
2020-09-30 05:07:16 +0530

    tweak app

--- Terton_FC/settings.py
+++ Terton_FC/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'website',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -107,6 +108,19 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+
+
+
+AWS_ACCESS_KEY_ID = 'AKIAUD2U2BROZJOE4NTQ'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'tertonfc'
+AWS_S3_FILE_OVERWRITE=False
+AWS_DEFAULT_ACL=None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+
+AWS_S3_REGION_NAME = 'ap-south-1'
 
 
 # Static files (CSS, JavaScript, Images)
